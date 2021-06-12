2740370c1a809a8dcd39b4c627c59c51ed6473c7
salahzaki <salahzaki999@gmail.com>
2021-06-12 22:24:04 +0300

    shop

---
+++ README.md
@@ -0,0 +1,16 @@
+# Gadgets Store App UI
+
+## [Watch it on YouTube](https://www.youtube.com/watch?v=QCFgrZyt86M)
+
+
+Gadgets Store app UI using flutter,This project consist of Three Screens.
+
+### :heart: Found this project useful?
+
+If you found this project useful, then please consider giving it a :star: on Github and follow me on GitHub.
+
+### Gadgets Store App UI
+
+![App UI](/gadgetsallscr.png)
+![App UI](/gadgetsthumb.png)
+
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:gadgets_store_app/constants/constants.dart';
+import 'package:gadgets_store_app/screen/home/home_screen.dart';
+import 'package:gadgets_store_app/screen/navigation_drawer/navigaton_drawer_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Harag  Store',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        scaffoldBackgroundColor: white
+      ),
+      home: NavigationDrawerScreen(),
+    );
+  }
+}
